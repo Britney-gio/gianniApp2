5c5b4811b9650dda9bed7e281805175ab62f7036
Utente <giorgianieli@gmail.com>
2026-02-15 14:59:19 +0100

    creo nuovo componente ScrollToTop ed in maniera globale quando cambio route automaticamenta velo ia parte top della pagina

--- gianniApp2/src/App.tsx
+++ gianniApp2/src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
